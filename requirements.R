6688e151287de746afb9f89b201a345e160dce7d
Goran Udosic <gor181@users.noreply.github.com>
2019-01-28 15:04:19 +0100

    update requirements.R contents

--- requirements.R
+++ requirements.R
@@ -1,1 +1,1 @@
-# https://assets.datacamp.com/development/repositories/143/datasets/17b36e432849e80ec800d3751ff95e1930683ffb/kristof.jpg
+# https://assets.datacamp.com/development/repositories/143/datasets/e74cffa7aeb7c670d1da44aa2e48b568e8057a9e/kristof.jpg
